1f2b670a2ac77160384f09c770ae338ac110a4bb
Den <denisgavrilov395@gmail.com>
2021-04-12 04:16:50 +1000

    lessons4 task3 homework

---
+++ task3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main() {
+    int a;
+    int b;
+    int sum;
+    std::cout << "Enter a number 1\n";
+    std::cin >> a;
+    std::cout << "Enter a number 2\n";
+    std::cin >> b;
+    sum = a+b;
+         if (a and b==10 or sum==10)
+            std::cout << "This is true a,b=10 ot sum=10\n";
+         else if (a and b!=10 or sum!=10) {
+             std::cout << "This is not true a,b!=10 or sum!=10\n";
+         }
+         return 0;
+
+
+}
